e6bf2afbbc538022f7a523d12e040b4f0c40d9c2
VARUN SUNKARA <varunkumarsunkara39@gmail.com>
2025-07-08 17:48:56 +0530

    /

--- src/pages/Pack365Payment.tsx
+++ src/pages/Pack365Payment.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
