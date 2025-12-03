4afa49ee157a3b7908f54ac9359d9d5f4a642320
NaveenKorikana <NaveenKorikana@flyhii.in>
2025-12-03 12:57:59 +0530

    Update StudentDashboard.tsx

--- src/components/dashboards/StudentDashboard.tsx
+++ src/components/dashboards/StudentDashboard.tsx
@@ -28,7 +28,7 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import APStudentDashboard from '@/components/student/APStudentDashboard';
 import ApplyInternshipDialog from '@/components/internships/ApplyInternshipDialog';
-import StudentCourseManagement from './StudentCourseManagement'; // Import the new component
+import StudentCourseManagement from '@/components/student/StudentCourseManagement'; 
 
 interface Job {
   _id: string;
